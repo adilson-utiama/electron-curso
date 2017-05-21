ce8e811b312c83fd17e8f8f80331d806db198b11
Adilson <asu2u@yahoo.com.br>
2017-05-21 19:31:03 -0300

    Shell e process

--- app/js/sobre.js
+++ app/js/sobre.js
@@ -1,7 +1,18 @@
-const { ipcRenderer } = require('electron');
+const { ipcRenderer, shell } = require('electron');
+const process = require('process');
 
 let linkFechar = document.querySelector('#link-fechar');
+let linkTwitter = document.querySelector('#link-twitter');
+let electronVersion = document.querySelector('#electron-version');
+
+window.onload = function(){
+    electronVersion.textContent = process.versions.electron;
+}
 
 linkFechar.addEventListener('click', function(){
     ipcRenderer.send('fechar-janela-sobre');
+});
+
+linkTwitter.addEventListener('click', function(){
+    shell.openExternal('https://twitter.com');
 });
