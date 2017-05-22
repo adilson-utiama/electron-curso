7dbad970a1e948bc6c36dc497930d85a6d2da536
Adilson <asu2u@yahoo.com.br>
2017-05-21 21:15:39 -0300

    Implementando o timer - botao stop

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -10,8 +10,16 @@ linkSobre.addEventListener('click' , function(){
 });
 
 let imgs = ['img/play-button.svg', 'img/stop-button.svg'];
+let play = false;
 botaoPlay.addEventListener('click', function() {
+    if(play){
+        timer.parar();
+        play = false;
+    }else{
+        timer.iniciar(tempo);
+        play = true;
+    }
     imgs = imgs.reverse();
     botaoPlay.src = imgs[0];
-    timer.iniciar(tempo);
+
 });
