8b67d2f5eec37c4afb7e18caef94e290bb567e8a
Adilson <asu2u@yahoo.com.br>
2017-05-21 21:04:09 -0300

    Implementando o timer - botao start

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -1,7 +1,9 @@
 const { ipcRenderer } = require('electron');
+const timer = require('./timer');
 
 let linkSobre = document.querySelector('#link-sobre');
 let botaoPlay = document.querySelector('.botao-play');
+let tempo = document.querySelector('.tempo');
 
 linkSobre.addEventListener('click' , function(){
     ipcRenderer.send('abrir-janela-sobre');
@@ -11,4 +13,5 @@ let imgs = ['img/play-button.svg', 'img/stop-button.svg'];
 botaoPlay.addEventListener('click', function() {
     imgs = imgs.reverse();
     botaoPlay.src = imgs[0];
+    timer.iniciar(tempo);
 });
